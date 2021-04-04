62c9e5bed2d7c58e088267c4f72335e98bca18f0
mathiashansen <hansen.dk@hotmail.com>
2021-04-04 11:25:10 +0200

    Initial Commit

---
+++ Shared/NewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewProjectApp.swift
+//  Shared
+//
+//  Created by Mathias Hansen on 04/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct NewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
